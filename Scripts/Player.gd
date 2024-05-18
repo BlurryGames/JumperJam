2140bd5b96d34ff6961ba0d5643c7b6d0f03c973
Blurry <julian.lb.games@gmail.com>
2024-05-18 10:21:15 +0200

    Section2: Mobile Gameplay / Lesson5: Adding Gravity

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,17 @@ class_name Player extends CharacterBody2D
 var viewportSize: Vector2 = Vector2.ZERO
 
 var speed: float = 300.0
+var gravity: float = 15.0
+var maxFallVelocity: float = 1000.0
 
 func _ready()-> void:
 	viewportSize = get_viewport_rect().size
 
-func _physics_process(delta: float)-> void:
+func _physics_process(_delta: float)-> void:
+	velocity.y += gravity
+	if velocity.y > maxFallVelocity:
+		velocity.y = maxFallVelocity
+	
 	var direction: float = Input.get_axis("MoveLeft", "MoveRight")
 	if direction:
 		velocity.x = direction * speed
