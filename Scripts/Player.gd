2c4c0d36a36b745efbebc36670488885e85612aa
Blurry <julian.lb.games@gmail.com>
2024-05-18 10:13:10 +0200

    Section2: Mobile Gameplay / Lesson4: Teleporting On Edges

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,11 @@
 class_name Player extends CharacterBody2D
 
+var viewportSize: Vector2 = Vector2.ZERO
+
 var speed: float = 300.0
+
+func _ready()-> void:
+	viewportSize = get_viewport_rect().size
 
 func _physics_process(delta: float)-> void:
 	var direction: float = Input.get_axis("MoveLeft", "MoveRight")
@@ -10,3 +15,9 @@ func _physics_process(delta: float)-> void:
 		velocity.x = move_toward(velocity.x, 0.0, speed)
 	
 	move_and_slide()
+	
+	var margin: float = 20.0
+	if global_position.x > viewportSize.x + margin:
+		global_position.x = -margin
+	elif global_position.x < -margin:
+		global_position.x = viewportSize.x + margin
