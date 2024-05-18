8c8f4e803c1728b1f7a96f85ed4632d6553a6088
Blurry <julian.lb.games@gmail.com>
2024-05-18 11:27:22 +0200

    Section2: Mobile Gameplay / Lesson9: Camera Limits

--- Scripts/GameCamera.gd
+++ Scripts/GameCamera.gd
@@ -2,8 +2,21 @@ class_name GameCamera extends Camera2D
 
 var player: Player = null
 
+var viewportSize: Vector2 = Vector2.ZERO
+
 func _ready()-> void:
-	global_position.x = get_viewport_rect().size.x * 0.5
+	viewportSize = get_viewport_rect().size
+	global_position.x = viewportSize.x * 0.5
+	
+	limit_bottom = viewportSize.y
+	limit_left = 0.0
+	limit_right = viewportSize.x
+
+func _process(_delta: float)-> void:
+	if player:
+		var limitDistance: float = 420.0
+		if limit_bottom > player.global_position.y + limitDistance:
+			limit_bottom = player.global_position.y + limitDistance
 
 func _physics_process(_delta: float)-> void:
 	if player:
