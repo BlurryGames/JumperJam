d2d2d371984c63f354d6dac4cf55e7eea0f42960
Blurry <julian.lb.games@gmail.com>
2024-05-18 19:58:05 +0200

    Section2: Mobile Gameplay / Lesson15: Level Gen Script

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,66 +1,22 @@
 class_name Game extends Node2D
 
-@onready var platformParent: Node2D = $PlatformParent
+@onready var levelGenerator: LevelGenerator = $LevelGenerator
 @onready var player: Player = $Player
 
 var cameraScene: PackedScene = preload("res://scenes/game_camera.tscn")
-var platformScene: PackedScene = preload("res://scenes/platform.tscn")
 
 var camera: GameCamera = null
-
-var viewportSize: Vector2 = Vector2.ZERO
-
-var startPlatformY: float = 0.0
-var yDistanceBetweenPlatforms: float = 100.0
-
-var levelSize: int = 10
-var generatedPlatformCount: int = 0
 
 func _ready()-> void:
 	camera = cameraScene.instantiate()
 	camera.setupCamera($Player)
 	add_child(camera)
 	
-	viewportSize = get_viewport_rect().size
-	generatedPlatformCount = 0
-	startPlatformY = viewportSize.y - (yDistanceBetweenPlatforms * 2.0)
-	generateLevel(startPlatformY, true)
+	if player:
+		levelGenerator.setup(player)
 
 func _process(_delta: float)-> void:
 	if Input.is_action_just_pressed("Quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
-	
-	if player:
-		var playerPositionY: float = player.global_position.y
-		var endOfLevelPosition: float = startPlatformY - (generatedPlatformCount * yDistanceBetweenPlatforms)
-		var threshold: float = endOfLevelPosition + (yDistanceBetweenPlatforms * 6.0)
-		if playerPositionY <= threshold:
-			generateLevel(endOfLevelPosition, false)
-
-func createPlatform(location: Vector2)-> Platform:
-	var platform: Platform = platformScene.instantiate()
-	platform.global_position = location
-	platformParent.add_child(platform)
-	
-	return platform
-
-func generateLevel(startY: float, generateGround: bool)-> void:
-	var platformWidth: float = 136.0
-	
-	if generateGround:
-		var groundLayerPlatformCount: float = (viewportSize.x / platformWidth) + 1.0
-		var groundLayerOffsetY: float = 62.0
-		for i: int in range(groundLayerPlatformCount):
-			var groundLocation: Vector2 = Vector2(i * platformWidth, viewportSize.y - groundLayerOffsetY)
-			createPlatform(groundLocation)
-	
-	for i: int in range(levelSize):
-		var maxPositionX = viewportSize.x - platformWidth
-		var randomX = randf_range(0.0, maxPositionX)
-		var location: Vector2 = Vector2.ZERO
-		location.x = randomX
-		location.y = startY - (yDistanceBetweenPlatforms * i)
-		createPlatform(location)
-		generatedPlatformCount += 1
