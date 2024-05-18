dafb7abca7b6e6b9300dc8eccaedc804d6750d6c
Blurry <julian.lb.games@gmail.com>
2024-05-18 10:56:09 +0200

    Section2: Mobile Gameplay / Lesson7: Player Jump

--- Scripts/Platform.gd
+++ Scripts/Platform.gd
@@ -2,4 +2,5 @@ class_name Platform extends Area2D
 
 func _on_body_entered(body: Node2D)-> void:
 	if body is Player:
-		pass
+		if body.velocity.y > 0.0:
+			body.jump()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,24 @@
 class_name Player extends CharacterBody2D
 
+@onready var animator: AnimationPlayer = $AnimationPlayer
+
 var viewportSize: Vector2 = Vector2.ZERO
 
 var speed: float = 300.0
 var gravity: float = 15.0
 var maxFallVelocity: float = 1000.0
+var jumpVelocity: float = -800.0
 
 func _ready()-> void:
 	viewportSize = get_viewport_rect().size
+
+func _process(_delta: float)-> void:
+	if velocity.y > 0.0:
+		if animator.current_animation != "Fall":
+			animator.play("Fall")
+	elif velocity.y < 0.0:
+		if animator.current_animation != "Jump":
+			animator.play("Jump")
 
 func _physics_process(_delta: float)-> void:
 	velocity.y += gravity
@@ -27,3 +38,6 @@ func _physics_process(_delta: float)-> void:
 		global_position.x = -margin
 	elif global_position.x < -margin:
 		global_position.x = viewportSize.x + margin
+
+func jump()-> void:
+	velocity.y = jumpVelocity
