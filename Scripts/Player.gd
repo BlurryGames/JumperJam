083570e237933c5b20723061636672dbcd414e99
Blurry <julian.lb.games@gmail.com>
2024-05-19 17:56:00 +0200

    Section3: Exporting And Testing / Lesson7: Accelerometer Input

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,12 +5,20 @@ class_name Player extends CharacterBody2D
 var viewportSize: Vector2 = Vector2.ZERO
 
 var speed: float = 300.0
+var accelerometerSpeed: float = 130.0
+
 var gravity: float = 15.0
 var maxFallVelocity: float = 1000.0
 var jumpVelocity: float = -800.0
 
+var useAccelerometer: bool = false
+
 func _ready()-> void:
 	viewportSize = get_viewport_rect().size
+	
+	var osName: String = OS.get_name()
+	if osName == "Android" or osName == "iOS":
+		useAccelerometer = true
 
 func _process(_delta: float)-> void:
 	if velocity.y > 0.0:
@@ -25,11 +33,15 @@ func _physics_process(_delta: float)-> void:
 	if velocity.y > maxFallVelocity:
 		velocity.y = maxFallVelocity
 	
-	var direction: float = Input.get_axis("MoveLeft", "MoveRight")
-	if direction:
-		velocity.x = direction * speed
+	if useAccelerometer:
+		var mobileInput: Vector3 = Input.get_accelerometer()
+		velocity.x = mobileInput.x * accelerometerSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0.0, speed)
+		var direction: float = Input.get_axis("MoveLeft", "MoveRight")
+		if direction:
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0.0, speed)
 	
 	move_and_slide()
 	
