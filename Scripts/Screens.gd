821baa8b63eb662e4cd741a1717c676e67df6939
Blurry <julian.lb.games@gmail.com>
2024-05-19 21:05:18 +0200

    Section4: UI System / Lesson7: Handling Buttons

--- Scripts/Screens.gd
+++ Scripts/Screens.gd
@@ -4,6 +4,30 @@ class_name Screens extends CanvasLayer
 
 func _ready()-> void:
 	console.visible = false
+	
+	registerButtons()
 
 func _on_toggle_console_pressed()-> void:
 	console.visible = not console.visible
+
+func _on_button_pressed(button: TextureButton)-> void:
+	match button.name:
+		"TitlePlay":
+			print("PlayPress")
+		"PauseRetry":
+			print("PauseRetryPress")
+		"PauseBack":
+			print("PauseBackPress")
+		"PauseClose":
+			print("PauseClosePress")
+		"GameOverRetry":
+			print("GameOverRetryPress")
+		"GameOverBack":
+			print("GameOverBackPress")
+
+func registerButtons()-> void:
+	var buttons: Array[Node] = get_tree().get_nodes_in_group("Buttons")
+	if buttons.size() > 0:
+		for b: Node in buttons:
+			if b is ScreenButton:
+				b.clicked.connect(_on_button_pressed)
