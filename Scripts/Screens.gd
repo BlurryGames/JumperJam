fd932328ec327fdf3585ba0d8523622722fb3e72
Blurry <julian.lb.games@gmail.com>
2024-05-19 18:43:52 +0200

    Section4: UI System / Lesson2: Debug Console

--- Scripts/Screens.gd
+++ Scripts/Screens.gd
@@ -1,1 +1,9 @@
 class_name Screens extends CanvasLayer
+
+@onready var console: Control = $Debug/ConsoleLog
+
+func _ready()-> void:
+	console.visible = false
+
+func _on_toggle_console_pressed()-> void:
+	console.visible = not console.visible
