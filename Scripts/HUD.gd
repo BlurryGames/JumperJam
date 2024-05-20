985efe15f1595d6e7904a4e0e8b07dde60098c84
Blurry <julian.lb.games@gmail.com>
2024-05-20 17:07:11 +0200

    Section4: UI System / Lesson13: Screen Scale

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -5,9 +5,16 @@ class_name HUD extends Control
 
 func _ready()-> void:
 	var osName: String = OS.get_name()
+	
 	if osName == "Android" or osName == "iOS":
 		var safeArea: Rect2i = DisplayServer.get_display_safe_area()
 		var safeAreaTop: int = safeArea.position.y
+		
+		if osName == "iOS":
+			var screenScale: float = DisplayServer.screen_get_scale()
+			safeAreaTop /= screenScale
+			
+			UtilityPtr.addLogMessage("Screen scale: " + str(screenScale))
 		
 		topBar.position.y += safeAreaTop
 		var margin: int = 10
